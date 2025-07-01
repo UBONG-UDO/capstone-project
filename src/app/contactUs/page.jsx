dc10e0ed51c01f7b434726ad204fdd3e5e3eb674
UBONG-UDO <udoubong123@gmail>
2025-07-01 15:36:13 -0700

    Build: FontAwesome and Radix UI installation

--- src/app/contactUs/page.jsx
+++ src/app/contactUs/page.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
-// import { Input } from '@/components/ui/input';
+import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
