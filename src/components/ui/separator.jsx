4d6df793558f4dc5c73ccaf75dddca3fb788af86
UBONG-UDO <udoubong123@gmail>
2025-07-05 08:07:49 -0700

    FIx: Added Checkout page

--- src/components/ui/separator.jsx
+++ src/components/ui/separator.jsx
@@ -1,8 +1,7 @@
 'use client';
 
 import * as React from 'react';
-// import * as SeparatorPrimitive from '@radix-ui/react-separator';
-import { Separator } from '@radix-ui/react-separator';
+import * as SeparatorPrimitive from '@radix-ui/react-separator'; // Import as namespace
 import { cn } from '@/lib/utils';
 
 const Separator = React.forwardRef(function Separator(
@@ -27,3 +26,34 @@ const Separator = React.forwardRef(function Separator(
 Separator.displayName = SeparatorPrimitive.Root.displayName;
 
 export { Separator };
+
+
+// 'use client';
+
+// import * as React from 'react';
+// // import * as SeparatorPrimitive from '@radix-ui/react-separator';
+// import { Separator } from '@radix-ui/react-separator';
+// import { cn } from '@/lib/utils';
+
+// const Separator = React.forwardRef(function Separator(
+//   { className, orientation = 'horizontal', decorative = true, ...props },
+//   ref
+// ) {
+//   return (
+//     <SeparatorPrimitive.Root
+//       ref={ref}
+//       decorative={decorative}
+//       orientation={orientation}
+//       className={cn(
+//         'shrink-0 bg-border',
+//         orientation === 'horizontal' ? 'h-[1px] w-full' : 'h-full w-[1px]',
+//         className
+//       )}
+//       {...props}
+//     />
+//   );
+// });
+
+// Separator.displayName = SeparatorPrimitive.Root.displayName;
+
+// export { Separator };
